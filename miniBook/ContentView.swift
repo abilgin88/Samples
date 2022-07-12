18cbcf98a2d9e22dcf6dc674d74b7b5d11b97731
Abdullah Bilgin <bilgin.abdullah@icloud.com>
2022-07-12 11:11:12 +0300

    Initial Commit

---
+++ miniBook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  miniBook
+//
+//  Created by Abdullah Bilgin on 7/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ miniBook/miniBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  miniBookApp.swift
+//  miniBook
+//
+//  Created by Abdullah Bilgin on 7/12/22.
+//
+
+import SwiftUI
+
+@main
+struct miniBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
