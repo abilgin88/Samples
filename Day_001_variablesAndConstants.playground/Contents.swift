0273af8d37013c94cfb9f1475418e775a561b45a
Abdullah Bilgin <abilgin02@icloud.com>
2022-12-11 21:29:01 +0300

    Day_001_variablesAndConstants

---
+++ Day_001_variablesAndConstants.playground/Contents.swift
@@ -0,0 +1,115 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+var name = "Ted"
+name = "Rebecca"
+name = "Keeley"
+
+let character = "Daphne"
+//character = "Eloise"
+//character = "Francesca"
+
+
+var playerNAme = "Roy"
+print(playerNAme)
+
+playerNAme = "DAni"
+
+playerNAme = "Sam"
+
+let managerName =  "Michael Scott"
+let dogBreed = " Samoyed"
+let meaningOfLife = "How many roads must a man walk down?"
+
+let actor = "Denzel Washington"
+
+
+let filename = "paris.jpg"
+let result = " ⭐️you win! ⭐️"
+let quote = "Then he tapped a sing saying \"Belive\" and walked away"
+
+let movie = """
+A day in
+the life of an
+Apple engineer
+"""
+
+print(actor.count)
+
+let nameLenght = actor.count
+print(nameLenght)
+print(result.uppercased())
+
+
+print(movie.hasPrefix("A day"))
+print(filename.hasPrefix(".jpg"))
+
+var quote1 = "Change the world by being yourself"
+
+var burns = """
+The best laid schemes
+O' mice and men
+Gang aft agley
+"""
+
+let score = 10
+let reallyBig = 100000000
+let reallybig2 = 100_000_000
+let reallybig3 = 1_00_00_00__0____0
+
+let lowerScore = score - 2
+let higherScore = score + 10
+let doubleScore = score * 2
+let squaredScore = score * score
+let halvedScore = score / 2
+
+print(score)
+
+var counter = 10
+counter = counter + 5
+counter += 5
+print(counter)
+
+counter *= 2
+print(counter)
+counter -= 10
+print(counter)
+counter /= 2
+print(counter)
+
+let number = 120
+print(number.isMultiple(of: 3))
+print(120.isMultiple(of: 4))
+
+
+let number2 = 0.1 + 0.2
+print(number2)
+
+let a = 1
+let b = 2.0
+let c = a + Int(b) // type safe
+
+let double1 = 2.1
+let double2 = 2121.2121
+let double3 = 3.0
+let Int1 = 3
+
+var name1 = "Nicolas Cage"
+name1 = "Jhon Travolta"
+
+var name2 = "Nicolas Cage"
+//var name2 = 57
+
+var raiting = 5.0
+raiting *= 2
+
+
+var myInt = 1
+var myDouble = 1.0
+
+
+var mainingOflife = 42
+
+
+//meaningOfLife = "Forty two"
